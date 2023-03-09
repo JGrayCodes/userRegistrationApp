0f2aaf0e1cc86685b8b15b2066a3a5bbd5d870f8
JGrayCodes <115024667+JGrayCodes@users.noreply.github.com>
2023-03-09 07:13:11 +0200

    Added one Function and Event Listener
    Corrected wording everywhere from document.getElementsByID to document.getElementById (removed "s" and changed case from ID to Id)
    Added function and event listener to show the users tab

--- src/js/user-register.js
+++ src/js/user-register.js
@@ -17,28 +17,28 @@ console.log(userTabs);
 /* --- Nav references --- */
 
 // The "users-tab-button" is the one that links to the "Register Users" tab
-const usersTabButton = document.getElementsByID("users-tab-button");
+const usersTabButton = document.getElementById("users-tab-button")
 console.log(usersTabButton);  
 
 // The "register-tab-button" is the one that links to the "View User List" tab
-const registerTabButton = document.getElementsByID("register-tab-button");
+const registerTabButton = document.getElementById("register-tab-button")
 console.log(registerTabButton);
 
 
 /* --- Form field references --- */
 
-const registerForm = document.getElementsByID("register-form");
-const registerFirstName = document.getElementsByID("firstname");
-const registerLastName = document.getElementsByID("lastname");
-const registerUserName = document.getElementsByID("username");
-const registerEmail = document.getElementsByID("email");
-const registerPassword = document.getElementsByID("password");
-const registerSubmit = document.getElementsByID("register-submit");
+const registerForm = document.getElementById("register-form")
+const registerFirstName = document.getElementById("firstname")
+const registerLastName = document.getElementById("lastname")
+const registerUserName = document.getElementById("username")
+const registerEmail = document.getElementById("email")
+const registerPassword = document.getElementById("password")
+const registerSubmit = document.getElementById("register-submit")
 
 
 /* --- Table list reference --- */
 
-const userList = document.getElementsByID("user-list");
+const userList = document.getElementById("user-list");
 const userListHeader = document.getElementsByClassName("header");
 const userListEntry = document.getElementsByClassName("entry");
 
@@ -47,10 +47,14 @@ const userListEntry = document.getElementsByClassName("entry");
     Functionality
 ---------------------------------------------------- */
 
-// code here...
-
-
+const showUsers = () => {
+    userTabs[0].style.display = "block";
+    registerTab[0].style.display = "none";
+    userTabs[1].style.display = "block";
+    registerTab[1].style.display = "none";
+}
 
 /* ----------------------------------------------------
     Event Listeners and Interactivity
 ---------------------------------------------------- */
+registerTabButton.addEventListener("click", showUsers);
